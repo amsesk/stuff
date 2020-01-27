292fcbfbf4f04e72d524d0fce783dcb3709f6cbe
amsesk <amsesk@umich.edu>
2020-01-27 18:10:47 -0500

    the actual scripts

--- ploidy_hists_mapping.py
+++ ploidy_hists_mapping.py
@@ -8,16 +8,18 @@ import plotly.express as px
 import plotly
 import pandas as pd
 import numpy as np
+import sys
+import argparse
+import os
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--freqs", metavar="allele_frequences", required=True, action="store")
+args = parser.parse_args()
 
-# In[2]:
-
+f = args.freqs
+prefix = os.path.split(f)[1].split(".")[0] 
 
 data= []
-
-#f = "66955_S288C_AD.vcf"
-#f = "69521_S288C_AD.vcf"
-f = "para_AD"
 
 with open(f, 'r') as ad:
     for line in ad:
@@ -26,39 +28,17 @@ with open(f, 'r') as ad:
         else:
             data.append([x.strip() for x in line.split(",")])
 
-
-# In[3]:
-
-
 freqs = pd.DataFrame(data, columns=["ref_count", "var_count", "NON_REF"], dtype=float)
-
-
-# In[4]:
-
-
 
 freqs["total"] = freqs.apply(lambda row: row.ref_count+row.var_count+row.NON_REF, axis=1)
 freqs["ref_af"] = freqs.apply(lambda row: np.true_divide(row.ref_count, row.total), axis=1)
 freqs["var_af"] = freqs.apply(lambda row: np.true_divide(row.var_count, row.total), axis=1)
 
-
-# In[5]:
-
+freqs = freqs[ (freqs["ref_af"] > 0) & (freqs["ref_af"] < 1) ] 
 
 hist = px.histogram( x=pd.concat([freqs.ref_af, freqs.var_af]), nbins = 1000)
-hist.show()
-
-
-# In[13]:
-
+#hist.show()
 
 plotly.io.orca.config.executable = '/usr/local/bin/orca'
-hist.write_image(format = "pdf", file="para_af.pdf")
-#plotly.offline.plot(hist, filename='para_af.html', auto_open=False)
-
-
-# In[ ]:
-
-
-
+hist.write_image(format = "pdf", file=f"{prefix}_af.pdf")
 
